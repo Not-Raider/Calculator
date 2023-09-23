a71040b0e7724ca3e2abafe92d539dddd58d4fd1
Not-Raider <145824200+Not-Raider@users.noreply.github.com>
2023-09-23 10:06:11 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,45 +1,20 @@
-let string = "";
-let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach((button)=>{
-button.addEventListener('click', (e)=>{
-    if(e.target.innerHTML == '='){
-        string = eval(string);
-        document.querySelector('input').value = string;
-    }
-    else{
-        console.log(e.target);
-        string = string + e.target.innerHTML;
-        document.querySelector('input').value = string;
-    }
-    if(e.target.innerHTML == 'AC'){
-        string = eval(0);
-        document.querySelector('input').value = string;
-    }else{
-        console.log(e.target)
-    }
-    if(e.target.innerHTML == '*'){
-        string = eval('*');
-        document.querySelector('input').value = string;
-    }else{
-        console.log(e.target);
-    }
-    if(e.target.innerHTML == '+'){
-        string = eval('+');
-        document.querySelector('input').value = string;
-    }else{
-        console.log(e.target);
-    }
-    if(e.target.innerHTML == '-'){
-        string = eval('-');
-        document.querySelector('input').value = string;
-    }else{
-        console.log(e.target);
-    }
-    if(e.target.innerHTML == '/'){
-        string = eval('/');
-        document.querySelector('input').value = string;
-    }else{
-        console.log(e.target);
-    }
-})
+let user_input = document.getElementById('user-input');
+let button = document.querySelectorAll('button');
+
+button.forEach(element =>{
+    element.addEventListener('click', (e) =>{
+        console.log(e.target.textContent);
+        if(e.target.textContent === 'C'){
+            user_input.innerHTML ="";
+        }else if(e.target.textContent === 'X'){
+            user_input.innerHTML = user_input.innerHTML.slice(0,-1);
+        }else if(e.target.textContent === '='){
+            user_input.innerHTML = eval(user_input.innerHTML);
+        }
+        
+        else{
+            user_input.innerHTML += e.target.textContent 
+        }
+        user_input.scrollLeft = user_input.scrollWidth
+    })
 })
